ff85a5150d034e0164959ce962f3288598f3c1f4
Egor Kruglov <yegorkruglov@gmail.com>
2023-04-21 13:01:49 +0300

    setup appearance and logic

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var activeView: UIView?
+    
+    @IBOutlet var redView: UIView!
+    @IBOutlet var orangeView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redView.layer.cornerRadius = redView.frame.height / 2
+        orangeView.layer.cornerRadius = redView.frame.height / 2
+        greenView.layer.cornerRadius = redView.frame.height / 2
+        startButton.layer.cornerRadius = 20
     }
-
-
+    
+    @IBAction func startButtonTapped() {
+        startButton.setTitle("NEXT", for: .normal)
+    
+        switch activeView {
+        case redView:
+            redView.alpha = 0.3
+            activeView = orangeView
+            activeView?.alpha = 1
+        case orangeView:
+            orangeView.alpha = 0.3
+            activeView = greenView
+            activeView?.alpha = 1
+        case greenView:
+            greenView.alpha = 0.3
+            activeView = redView
+            activeView?.alpha = 1
+        default:
+            activeView = redView
+            activeView?.alpha = 1
+        }
+    }
+    
 }
-
